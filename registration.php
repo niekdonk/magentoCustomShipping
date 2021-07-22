14d0a3987da4902dbf2f9a4a20202b8ea2b1610d
NiekDonk <niek.van.de.donk@bastrucks.com>
2021-07-22 10:34:43 +0200

    name fix

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Bas_CustomShipping', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'bastrucks_magento-custom-shipping', __DIR__);
